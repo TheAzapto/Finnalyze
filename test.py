55dd9b6ee5bbe898a89020108b9c6193c50b1f82
Aayush Dutta <aayush.dutta22@st.niituniversity.in>
2025-11-27 13:29:29 +0530

    removed test.py file

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.post(
-    'http://localhost:5000/analyze',
-    json={"article": r"Vedanta poised for 16% annual growth in pre-tax earnings through FY28 on volume ramp-up"}
-)
-
-print(response.json())
