ec328e6a1819f91b79f273202b3d4a01c1ab94f6
TheAzapto <aayush.dutta22@st.niituniversity.in>
2026-03-08 15:06:10 +0530

    Isolate production build and API for Vercel deployment

--- LLM_Files/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.post(
-        'http://localhost:5000/analyze',
-        json={"article": r"24% rally in 3 months puts this bank stock on track for next target of ₹1,500"}
-        )
-
-for i in response.json():
-    print(i)
